8dc31f090ad5a7095cf7c2e685b4f9a18d976682
Deivid Almeida <deividalmeida365@gmail.com>
2023-01-14 17:24:12 -0300

    starting server

---
+++ services/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+
+const app = express();
+const PORT = 3000;
+
+app.get('/',(req, res) => {
+
+    res.send(`Node and express running on port: ${PORT}`);
+
+});
+
+app.listen(PORT, () => {
+    
+    console.log(`Your server is running on port: ${PORT}`);
+});
